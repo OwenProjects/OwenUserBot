2f1b9a92ad08842473ec0a10d50a8d185627ac70
Ber4t Sensei <73439036+Ber4tBey@users.noreply.github.com>
2022-03-21 23:00:52 +0300

    LATEST VERSİON NO MORE SUPPORT

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -80,7 +80,7 @@ if not LANGUAGE in ["EN", "TR", "AZ", "UZ", "DEFAULT"]:
     LANGUAGE = "DEFAULT"
     
 # Owen versiyon
-OWEN_VERSION = "v4.1"
+OWEN_VERSION = "v5.0"
 
 MAX_MESSAGE_SIZE_LIMIT = 4095
 # Telegram API KEY ve HASH
