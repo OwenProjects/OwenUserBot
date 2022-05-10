770aeabed7446cb859fd489954ffc4f36bb433ec
Erdem Bey <85256257+erdewbey@users.noreply.github.com>
2022-05-10 16:20:16 +0300

    Update adminsudo.py

--- userbot/modules/adminsudo.py
+++ userbot/modules/adminsudo.py
@@ -1,15 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# OwenUserBot - ErdewBey - ByMisakiMey - Midy
-#
-
-"""
-OwenUserBot
-"""
 
 from asyncio import sleep
 from os import remove
