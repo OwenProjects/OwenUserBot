cd27ddfe48d3839175f122752ab0aecb1611b80f
JackMedya <85175546+newname58q@users.noreply.github.com>
2022-06-18 23:51:39 +0300

    Update degistir.py

--- userbot/modules/degistir.py
+++ userbot/modules/degistir.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - ErdewBey - Midy
+# OwenUserBot - jackdanielssx
 
 import re
 import userbot.modules.sql_helper.mesaj_sql as sql
