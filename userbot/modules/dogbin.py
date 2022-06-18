2df8eec7410194bd31945e68f6ccdf35095f75e0
JackMedya <85175546+newname58q@users.noreply.github.com>
2022-06-18 23:52:34 +0300

    Update dogbin.py

--- userbot/modules/dogbin.py
+++ userbot/modules/dogbin.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# AOwenUserBot - ErdewBey - Midy
+# AOwenUserBot - jackdanielssx
 
 
 """ Dogbin ile etkileşim için komutlar içeren UserBot modülü(https://del.dog)"""
