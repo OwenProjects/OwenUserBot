02ef3daef68757a8beaa9c3db40fe67584ba0cb0
JackMedya <85175546+newname58q@users.noreply.github.com>
2022-07-07 14:22:54 +0300

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Owen UserBot - Erdem Bey
+# Owen UserBot - jackdanielssx
 
 
 """ Gereksiz mesajları temizlemek için UserBot modülü (genellikle spam veya ot). """
