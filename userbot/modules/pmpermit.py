24c2079444fa74e92f1da759fb7cf47c5bd9f3b4
JackMedya <85175546+newname58q@users.noreply.github.com>
2022-07-07 14:23:22 +0300

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# OwenUserBot - ErdewBey - Midy
+# OwenUserBot - jackdanielssx
 
 
 """ Kimin size özel mesaj gönderebileceğini kontrol altına almanızı sağlayan UserBot modülüdür. """
