4eb70f0a318157eb3110f1bca80c8e79c6a7f59f
JackMedya <85175546+newname58q@users.noreply.github.com>
2022-10-01 12:55:21 +0300

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2022 The OwenProjects.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+# OwenUserBot - JackDaNieLs
+#
+#
+
 
 """ AFK ile ilgili komutları içeren UserBot modülü """
 
